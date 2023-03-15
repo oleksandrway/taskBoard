f68b6a761f30d76b1264b85c272d1a6d730a2b08
oleksandrway <72298698+oleksandrway@users.noreply.github.com>
2023-03-15 21:19:57 +0200

    add @ alias to vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import WindiCSS from 'vite-plugin-windicss'
+import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),
@@ -13,5 +15,10 @@ export default defineConfig({
     }),
     WindiCSS(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 
 })
